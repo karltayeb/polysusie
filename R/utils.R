c5b379bd81f5e7d3fc590c4b4dbdd1392b515b13
Karl Tayeb <karl.tayeb7@gmail.com>
2023-04-19 12:52:21 -0500

    categorical kl bugfix

--- R/utils.R
+++ R/utils.R
@@ -8,7 +8,22 @@ softmax <- function(x){
   return(exp(x - lse(x)))
 }
 
-normal_kl <- function (mu, var, mu0 = 0, var0 = 1){
+compute_kl_gaussian <- function (mu, var, mu0 = 0, var0 = 1){
   kl <- 0.5 * (log(var0) - log(var) + var/var0 + (mu - mu0)^2/var0 - 1)
+  return(kl)
+}
+
+compute_kl_categorical <- function (alpha, pi=rep(1/length(alpha), length(alpha))){
+  kl <- sum(alpha * (log(alpha) - log(pi)), na.rm = T)
+  return(kl)
+}
+
+compute_kl_polynomial <- function(q, p){
+  f <- function(x){
+    qx <- evaluate_poly(q, x)
+    px <- evaluate_poly(p, x)
+    return(exp(qx) * (qx - px))
+  }
+  kl <- cubature::hcubature(f, -20, 20)$integral
   return(kl)
 }
