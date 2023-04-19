9ae435558c7661f1f0b1944083fd6265a3438660
Karl Tayeb <karl.tayeb7@gmail.com>
2023-04-19 12:51:51 -0500

    functions for evaluating moments of gaussian and log-polynomial densities

--- R/moments.R
+++ R/moments.R
@@ -1,1 +1,80 @@
-# Functions for computing moments
+# Functions for computing moments and normalizing polynomial log density
+
+evaluate_poly <- function(p, x){
+  pracma::polyval(rev(p), x)
+}
+
+plot_poly <- function(p, range=c(-5, 5)){
+  f <- function(x) {evaluate_poly(p, x)}
+  plot(f, xlim = range)
+}
+
+evaluate_exp_poly <- function(p, x){
+  exp(evaluate_poly(p, x))
+}
+
+make_exp_poly <- function(p){
+  f <- function(x){evaluate_exp_poly(p,x)}
+  return(f)
+}
+
+plot_exp_poly <- function(p, range=c(-5, 5)){
+  f <- make_exp_poly(p)
+  plot(f, xlim = range)
+}
+
+
+#' Normalize polynomial log density
+#'
+#' update intercept so that \int \exp \{f(x) \} dx = 1
+#' computed numerically via quadrature.
+#' where f(x) is a polynomial with coefficients given by argument `p`
+#' @param p polynomial coefficients ordered by increasing degree e.g. (p0, ... pM)
+normalize_polynomial_log_density <- function(p){
+  f <- make_exp_poly(p)
+  Z <- cubature::hcubature(f, -20, 20)$integral
+  p[1] <- p[1] - log(Z)
+  return(p)
+}
+
+
+make_moment_function <- function(p, k){
+  f <- function(x){x^k * evaluate_exp_poly(p,x)}
+  return(f)
+}
+
+#' Compute moment
+#'
+#' Compute the moments of a distribution with polynomial log-density
+#' @param p polynomial coefficients ordered by increasing degree e.g. (p0, ... pM)
+#'  Assumes p is normalized!
+#' @param k the moment to compute
+#' @returns E[X^k] = \int x^k exp(f(x)) dx
+compute_moment_polynomial <- function(p, k, range=c(-20,20)){
+  f <- make_moment_function(p, k)
+  cubature::hcubature(f, -20, 20)
+  mu_k <- cubature::hcubature(f, range[1], range[2])$integral
+  return(mu_k)
+}
+
+compute_moments_polynomial <- function(p, K, range=c(-20, 20)){
+  mu <- purrr::map_dbl(1:K, ~compute_moment_polynomial(p, .x, range))
+  return(c(1, mu))
+}
+
+#' Compute normal moments
+#'
+#' Compute first `k` moments of a normal distribution (including 0th moment)
+#' @param mu mean
+#' @param var variance
+#' @param k number of moments to copmute
+compute_normal_moments <- function(mu, var, k){
+  return(purrr::map_dbl(0:k, ~actuar::mnorm(.x, mu, sqrt(var))))
+}
+
+#' compute k moments for psi = xb, b ~ N(mu, var)
+compute_psi_moments <- function(x, b_moments){
+  M <- length(b_moments) - 1
+  psi_moments <- do.call(cbind, purrr::map(0:M, ~ (x^.x) * b_moments[.x + 1]))
+  return(psi_moments)
+}
