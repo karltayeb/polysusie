ed57c1c2cabe39360c76bdc550a406ca4803a923
Karl Tayeb <karl.tayeb7@gmail.com>
2023-05-16 08:41:34 -0500

    add function for computing kl[normal | poly]

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,6 @@
-lse <- matrixStats::logSumExp
+lse <- function(x){
+  matrixStats::logSumExp(x)
+}
 
 sigmoid <- function(x){
   return(1/(1 + exp(-x)))
@@ -28,6 +30,38 @@ compute_kl_polynomial <- function(q, p){
   return(kl)
 }
 
+#' Compute KL divergence from normal (quadratic) to polynomial distributions
+#'
+#' Compute KL divergence between normal distribution and a distribution with
+#' polynomial log-density. Faster that `compute_kl_polynomial` because we
+#' can analytically compute moments undert the normal distribution.
+#' @param mu mean of normal distribution
+#' @param var variance of normal distribution
+#' @param p vector of polynomial coefficients
+#' @param normalize whether to normalize the polynomial density--
+#'   note: can optimize without normalizing, but can return negative values
+#' @return the KL divergence KL[N(mu, var) | p]
+compute_kl_normal_poly <- function(mu, var, p, normalize=F){
+  if(normalize){
+    p <- polysusie:::normalize_polynomial_log_density(p)
+  }
+  K = length(p) - 1
+  moments <- c(1, compute_normal_moments2(mu, var, K))
+  kl <- - 0.5 * log(2 * pi * exp(1) * var) - sum(moments * p)
+  return(kl)
+}
+
+#' Make polynomial function
+#'
+#' Take a vector of coefficients and make a vectorized
+#' function that evaluates the polynomial
+#' @param p a vector of coefficients in increasing degree e.g. c(c0, c1, ... c_k)
+#' @return a function `f` such that f(x)
+#'  evaluates the polynomial with coefficients `p` at x
+polyf <- function(p){
+  f <- function(x){pracma::polyval(rev(p), x)}
+  return(f)
+}
 
 is_monotone <- function (v) {
   return(all(tail(v, -1) - head(v, -1) >= 0))
