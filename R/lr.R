9697aab79385a2d426d2f68603292e17fe7aa4a7
Karl Tayeb <karl.tayeb7@gmail.com>
2023-06-01 12:08:14 -0500

    option to use ridge to set center of approximation interval

--- R/lr.R
+++ R/lr.R
@@ -1,11 +1,17 @@
 #' @export
 logistic_polysusie<- function(X, y, L, prior_variance=1,
-                                left=-5, right=5, M=2,
+                                left=5, right=5, center=0, M=2,
                                 max_iter=100, tol=1e-5, gaussian=T){
   if(!gaussian){
     warning('Non-Gaussian variational approximation not yet implimented')
   }
-  A <- bernoulli_poly_approx(y, left, right, M)
+
+  # center approximations at ridge regression predictions
+  if(center == 'ridge'){
+    ridge <- glmnet::cv.glmnet(X, y, family='binomial')
+    center <- drop(predict(ridge, X, s='lambda.min'))
+  }
+  A <- bernoulli_poly_approx(y, center - left, center + right, M)
   fit <- polynomial_approximate_susie(A, X,
                                       prior_variance=prior_variance,
                                       L=L, max_iter = max_iter, tol=tol)
